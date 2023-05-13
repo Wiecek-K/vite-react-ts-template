b3c64daede9ec43a224f984bdb6e3c414913fd88
Wiecek-K <krystiandwiecek@gmail.com>
2023-05-13 21:08:21 +0200

    install eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,33 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "autofix",
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "autofix/no-debugger": "error",
+        "arrow-body-style": ["error", "as-needed"],
+        "react/self-closing-comp": ["error", {
+            "component": true,
+            "html": true
+          }],
+          "react-hooks/exhaustive-deps": "error"
+    }
 }
